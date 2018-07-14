d9dd046ee9ff7cae02578b832bfb1e55b2010839
Phong Pham Nguyen Quang <phamphongbsn@outlook.com>
2018-07-14 16:49:15 +0700

    Initializing

---
+++ src/main/java/vn/cit/labmanager/LabManagerApplication.java
@@ -0,0 +1,12 @@
+package vn.cit.labmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabManagerApplication.class, args);
+	}
+}
