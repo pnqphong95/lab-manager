ca0d38f1572b00d2adb80f4c3df655a79de4e211
Phong Pham Nguyen Quang <phamphongbsn@outlook.com>
2018-07-22 17:42:47 +0700

    Stylize scheduler

--- src/main/resources/lab-manager-components/scheduler/resources/js/lm-scheduler.js
+++ src/main/resources/lab-manager-components/scheduler/resources/js/lm-scheduler.js
@@ -1,5 +1,5 @@
 $(function () {
-    $('#calendar').fullCalendar({
+    $('#lm-scheduler').fullCalendar({
         schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
         locale: 'vi',
         height: 'auto',
@@ -11,17 +11,44 @@ $(function () {
                 slotDuration: { days: 1 }
             }
         },
+        slotLabelFormat: ['dddd\nDD/MM/YYYY'],
         loading: function (isLoading, view) {
-            $('#calendar').css('filter', 'blur(20px)');
+            $('#lm-scheduler').css('filter', 'blur(100px)');
             $('#loading-indicator').css('display', 'block');
         },
         eventAfterAllRender: function (view) {
-            $('#calendar').css('filter', 'blur(0px)');
+            $('#lm-scheduler').css('filter', 'blur(0px)');
             $('#loading-indicator').css('display', 'none');
         },
         resourceGroupField: 'shift',
-        resourceLabelText: ' ',
-        resourceAreaWidth: '12%',
-        resources: 'https://api.myjson.com/bins/1c0ud6'
+        resourceAreaWidth: '75px',
+        resourceColumns: [{
+                labelText: ' ',
+                field: 'labname'
+        }],
+        windowResize: function (view) {
+            $('#lm-scheduler').fullCalendar('rerenderEvents');
+        }, 
+        eventRender: function (event, element) {
+            element.css("border", "none");
+            element.css("font-size", "1em");
+            element.css("padding", "6px 0");
+            element.css("background-color", "#00abd2");
+            element.css("line-height", "1.5");
+        },
+        resources: 'http://pnqphong-scheduler.getsandbox.com/api/shift_labs',
+        events: function (start, end, timezone, callback) {
+            $.ajax({
+                url: 'http://pnqphong-scheduler.getsandbox.com/api/lab_events',
+                dataType: 'json',
+                data: {
+                    from: start.format('YYYY-MM-DD'),
+                    to: end.subtract(1, 'days').format('YYYY-MM-DD')
+                },
+                success: function (events) {
+                    callback(events);
+                }
+            });
+        }
     });
 });
