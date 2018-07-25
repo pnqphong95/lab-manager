9039b3a20e9a6678d301c5454291cb6268249cab
Phong Pham Nguyen Quang <phong.pham@axonactive.com>
2018-07-25 16:15:13 +0700

    Integrated to thymeleaf

---
+++ src/main/java/vn/cit/labmanager/controller/LabHomeController.java
@@ -0,0 +1,13 @@
+package vn.cit.labmanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LabHomeController {
+	
+	@RequestMapping(path = "/")
+    public String index() {
+        return "index";
+    }
+}
