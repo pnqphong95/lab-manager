312ab5fbdb28d72e54497214e30c3807c4f64b49
Phong Pham Nguyen Quang <phong.pham@axonactive.com>
2018-07-25 18:19:43 +0700

    Spring security allow access resources and permit all user access

---
+++ src/main/java/vn/cit/labmanager/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package vn.cit.labmanager.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/static/**");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.anonymous();
+	}
+	
+}
