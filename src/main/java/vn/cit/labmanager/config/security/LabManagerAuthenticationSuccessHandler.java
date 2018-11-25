c2e23e88c863559867b63d51898980f355e1a0e8
Phong Pham Nguyen Quang <phamphongbsn@outlook.com>
2018-11-25 10:37:52 +0700

    Remove log

--- src/main/java/vn/cit/labmanager/config/security/LabManagerAuthenticationSuccessHandler.java
+++ src/main/java/vn/cit/labmanager/config/security/LabManagerAuthenticationSuccessHandler.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.savedrequest.SavedRequest;
 
@@ -16,8 +15,6 @@ public class LabManagerAuthenticationSuccessHandler extends SimpleUrlAuthenticat
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-		System.out.println("User has authorities: " + userDetails.getAuthorities());
 		HttpSession session = request.getSession();
 	    SavedRequest savedRequest = (SavedRequest) session.getAttribute("SPRING_SECURITY_SAVED_REQUEST");
 	    String forwardUrl = request.getContextPath();
