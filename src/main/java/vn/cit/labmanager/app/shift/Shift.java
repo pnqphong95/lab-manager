35f5d8836ebf9a4bc8a0c6fddc8d5b4698e58c0c
Phong Pham Nguyen Quang <phamphongbsn@outlook.com>
2018-11-25 14:14:55 +0700

    Shift validation

--- src/main/java/vn/cit/labmanager/app/shift/Shift.java
+++ src/main/java/vn/cit/labmanager/app/shift/Shift.java
@@ -1,9 +1,11 @@
 package vn.cit.labmanager.app.shift;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 import org.hibernate.annotations.GenericGenerator;
 
@@ -20,7 +22,13 @@ public class Shift extends AuditableEntity {
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "system-uuid")
     @GenericGenerator(name = "system-uuid", strategy = "uuid2")
 	private String id;
+	
+	@Column(nullable = false)
+	@NotBlank
 	private String name;
+	
+	@Column(nullable = false)
+	@NotBlank
 	private String shortName;
 
 }
