b586100fc626ba8f02d7efbd6539d1e8001e40e1
Phong Pham Nguyen Quang <phamphongbsn@outlook.com>
2018-12-03 21:03:52 +0700

    Course can be added by lecturer

--- src/main/java/vn/cit/labmanager/app/course/CourseAdminController.java
+++ src/main/java/vn/cit/labmanager/app/course/CourseAdminController.java
@@ -2,8 +2,6 @@ package vn.cit.labmanager.app.course;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Optional;
 
 import org.apache.commons.lang3.StringUtils;
@@ -17,11 +15,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import vn.cit.labmanager.app.period.Period;
 import vn.cit.labmanager.app.period.PeriodService;
 import vn.cit.labmanager.app.subject.SubjectService;
-import vn.cit.labmanager.app.user.User;
 import vn.cit.labmanager.app.user.UserService;
 
 @Controller
-public class CourseController {
+public class CourseAdminController {
 
 	@Autowired
 	private CourseService service;
@@ -35,39 +32,38 @@ public class CourseController {
 	@Autowired
 	private PeriodService periodService;
 	
-	@RequestMapping(path = "/admin/mycourses")
+	@RequestMapping(path = "/admin/courses")
     public String index(Model model) {
 		Optional<Course> lastModifiedCourse = service.findTopByOrderByModifiedDesc();
 		String lastModification = lastModifiedCourse.map(Course::getModified)
 				.map(modified -> modified.format(DateTimeFormatter.ofPattern("hh':'mm a',' dd/MM/yyyy")))
 				.orElse(StringUtils.EMPTY);
 		
-		model.addAttribute("courses", service.findByLecturerAndCurrentPeriod(userService.getCurrentUser().orElse(null)));
+		model.addAttribute("courses", service.findAll());
 		model.addAttribute("lastModification", lastModification);
-        return "admin/mycourse/index";
+        return "admin/course/index";
     }
 	
-	@RequestMapping(path = "/admin/mycourses", method = RequestMethod.POST)
+	@RequestMapping(path = "/admin/courses", method = RequestMethod.POST)
     public String saveCourse(Course course) {
 		service.save(course);
-		return "redirect:/admin/mycourses";
+		return "redirect:/admin/courses";
     }
 	
-	@RequestMapping(path = "/admin/mycourses/add")
+	@RequestMapping(path = "/admin/courses/add")
     public String createCourse(Model model) {
 		Optional<Period> period = periodService.findBySpecifiedDate(LocalDate.now());
 		model.addAttribute("isCurrentPeriodCreated", period.isPresent());
 		if (period.isPresent()) {
 			model.addAttribute("course", new Course());
 	        model.addAttribute("subjects", subjectService.findAll());
-	        Optional<User> user = userService.getCurrentUser();
-	        model.addAttribute("users", user.isPresent() ? Arrays.asList(user.get()) : new ArrayList<>());
+	        model.addAttribute("users", userService.findAll());
 	        model.addAttribute("periods", periodService.findAll());
 		}
-        return "admin/mycourse/edit";   
+        return "admin/course/edit";   
     }
 	
-	@RequestMapping(path = "/admin/mycourses/edit/{id}")
+	@RequestMapping(path = "/admin/courses/edit/{id}")
     public String editCourse(@PathVariable(name = "id") String id, Model model) {
 		Optional<Period> period = periodService.findBySpecifiedDate(LocalDate.now());
 		model.addAttribute("isCurrentPeriodCreated", period.isPresent());
@@ -80,13 +76,13 @@ public class CourseController {
 	            model.addAttribute("periods", periodService.findAll());
 	        });
 		}
-        return "admin/mycourse/edit";   
+        return "admin/course/edit";   
     }
 	
-	@RequestMapping(path = "/admin/mycourses/delete/{id}")
+	@RequestMapping(path = "/admin/courses/delete/{id}")
     public String deleteCourse(@PathVariable(name = "id") String id) {
         service.delete(id);
-        return "redirect:/admin/mycourses";   
+        return "redirect:/admin/courses";   
     }
 
 }
